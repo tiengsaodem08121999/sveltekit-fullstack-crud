b8a693d68175a992d3690f0a87dd03d17666a164
QuyLV <quylvsplus-software.com>
2025-02-17 11:44:35 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+    import Authenticate from "../components/Authenticate.svelte";
+</script>
+<Authenticate/>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,121 @@
+<script>
+    let todoList = ['Do the grocreies'];
+    let currTodo = '';
+    let error = true;
+
+    function addTodo (){
+        error = false;
+        if(!currTodo){
+            error = true;
+            return;
+        }
+        todoList = [...todoList, currTodo];
+    }
+</script>
+
+<di class="mainContent">
+    <div class="headerContainer">
+        <h1>Todo list</h1>
+        <button> <i class="fa-regular fa-floppy-disk"></i> Save</button>
+    </div>
+    <main>
+        {#each todoList as todo, index}
+        <div class="todo">
+            {index + 1}. {todo}
+        </div>
+        {/each}
+    </main>
+    <div class={"enterTodo" + "errorBorder" }>
+        <input type="text" bind:value={currTodo}  placeholder="Add a new todo" />
+        <button>Add</button>
+    </div>
+</di>
+<style>
+    .mainContainer {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        gap: 24px;
+        padding: 24px;
+        width: 100%;
+        max-width: 1000px;
+        margin: 0 auto;
+    }
+
+    .headerContainer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .headerBtns {
+        display: flex;
+        align-items: center;
+        gap: 14px;
+    }
+
+    .headerContainer button {
+        background: #003c5b;
+        color: white;
+        padding: 10px 18px;
+        border: none;
+        border-radius: 4px;
+        font-weight: 700;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        cursor: pointer;
+    }
+
+    .headerContainer button i {
+        font-size: 1.1rem;
+    }
+
+    .headerContainer button:hover {
+        opacity: 0.7;
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        flex: 1;
+    }
+
+    .enterTodo {
+        display: flex;
+        align-items: stretch;
+        border: 1px solid #0891b2;
+        border-radius: 5px;
+        overflow: hidden;
+    }
+
+    .errorBorder {
+        border-color: coral !important;
+    }
+
+    .enterTodo input {
+        background: transparent;
+        border: none;
+        padding: 14px;
+        color: white;
+        flex: 1;
+    }
+
+    .enterTodo input:focus {
+        outline: none;
+    }
+
+    .enterTodo button {
+        padding: 0 28px;
+        background: #003c5b;
+        border: none;
+        color: cyan;
+        font-weight: 600;
+        cursor: pointer;
+    }
+
+    .enterTodo button:hover {
+        background: transparent;
+    }
+</style>
